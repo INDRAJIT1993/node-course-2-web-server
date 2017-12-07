d0efc4d60ce1c383ae63953fe292a57910efdec7
Indrajit Singh <indrajeet654@gmail.com>
2017-12-07 13:42:27 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs =require('fs');
+var app = express();
+hbs.registerPartials(__dirname +'/views/partials')
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+app.use((req,res,next) =>{
+    var now = new Date().toString();
+    var log=(`${now}: ${req.method} ${req.url}`);
+    console.log(log);
+ fs.appendFile('consolelog', log + '\n');
+    next();
+});
+
+app.use((req,res,next) =>{
+res.render('maintianance.hbs');
+});
+hbs.registerHelper('getCurrentYear', () =>{
+    return new Date().getFullYear()
+});
+hbs.registerHelper('screamIt', (text) =>{
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) =>{
+res.render('home.hbs', {
+    pagetitle : 'home page',
+    bodytext :'Welcome',
+    
+
+});
+});
+
+app.listen(3000, () =>{
+    console.log('server is up and running at port 3000');
+});
+
+app.get('/about', (req, res) =>{
+res.render('about.hbs', {
+pagetitle : 'About page',
+
+});
+
+});
+
+app.get('/bad', (req, res) =>{
+res.send({
+    errormsg:'unable to handle request'
+});
+});
